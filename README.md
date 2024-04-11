636ccb7fc28aa3ef0a0137e04f5435eafa410809
Mabon Josephsen <catwings76@gmail.com>
2024-04-11 08:48:08 -0700

    Messed my commiting so just re connecting my repo

--- README.md
+++ README.md
@@ -13,3 +13,4 @@ There will be a button for creating a file with a name of your choosing.
 Then the get prompts button will be able to choose which file/files (I haven't decided if I should make it so you can just choose 1 or all or if you can specify multiple specific files) you will get x random prompts from.
 
 This is going to be a lot of changes, so I guess I'll slowly upload them to github under the branch version-1.1.0
+"# PromptGenerator" 
--- src/PromptGenerator.java
+++ src/PromptGenerator.java
@@ -8,4 +8,65 @@ public class PromptGenerator {
         gui.gui();
 
     }
+
+    /*
+    Create genre,	“genre to add”
+    Delete genre, “genre to delete”
+    Add prompt, “prompt to add”, A radio box for genre
+    Delete prompt, “prompt to delete”, A radio box for genre
+
+    This one is a bit weird. How should I implement it with multiple genres to account for?
+    1.Get X random prompts, “Number of prompts to get”, “A checklist for which genres.”
+    Getting x prompts from each genre
+
+    2.Get X random prompts, “Number of prompts to get”, “A checklist for which genres.”
+    Getting x prompts in total. But how would I decide how many from each genre?
+
+    Random chance? Imagine you get like 5 themes and nothing else?
+    I guess you could just reroll lol
+
+    Even distribution? What happens if you don’t have enough prompts requested for the lists? Does it error out? Just not get a prompt from each list? Get more prompts?
+
+    3.Get X random prompts, A checklist with a number next to each one for how many from each?
+    This a bit hard sounding not sure how I’d do it
+    Though honestly probably the best
+
+
+
+    Get all prompts, A checklist for which genres you want
+    Then output for each prompt like this?
+    “Genre: Genre prompt num: Prompt”,
+
+    For example
+
+    Themes: 1: Fantasy
+    Themes: 2: Sci-Fi
+    Themes: 3: Romance
+
+    Characters: 1: Sonic
+    Characters: 2: Mario
+    Characters: 3: Link
+
+    Or this?
+    “Genre”
+    “Prompt num: Prompt”
+
+    For example
+
+    Themes:
+    1: Fantasy
+    2: Sci-fi
+    3: Romance
+
+    Characters:
+    1: Sonic
+    2: Mario
+    3: Link
+
+    Finally on the last line
+    Get all genres,		Clear output
+     */
+
+
+
 }
